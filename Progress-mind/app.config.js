f517a4c9cbbc249f1dcaf845750312da62ab7c91
Gissela25 <gisselaverenice@gmail.com>
2022-11-09 23:15:53 -0600

    LastUpdate

--- Progress-mind/app.config.js
+++ Progress-mind/app.config.js
@@ -1,4 +1,3 @@
-import 'dotenv/config'
 
 export default
   {
@@ -34,14 +33,6 @@ export default
       },
       "web": {
         "favicon": "./assets/favicon.png"
-      },
-      "extra": {
-        firebaseApiKey: process.env.FIREBASE_API_KEY,
-        firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-        firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        firebaseAppId: process.env.FIREBASE_APP_ID
       },
     }
   }
